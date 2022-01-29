4833cb24a76ba5569b92662ebbb002cb0edfc6b2
Ireneusz Wyszomirski <ireneuszwyszomirski44@gmail.com>
2022-01-29 20:53:16 +0100

    clicked link active

--- js/script.js
+++ js/script.js
@@ -5,17 +5,27 @@
   });*/
 
   const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
-  
-    /* remove class 'active' from all article links  */
+    console.log("jaki argument dostała function?:", event);
+    console.log('clickedElement (with plus): '+clickedElement);
+
+    /* [DONE] remove class 'active' from all article links  */
+        
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
       activeLink.classList.remove('active');
-    }
-    /* add class 'active' to the clicked link */
+      }
+
+    /* [DONE] add class 'active' to the clicked link */
+    
+    clickedElement.classList.add("active")
+
+    console.log ("clickedElement:", clickedElement);
   
-    /* remove class 'active' from all articles */
+    /* [DONE]remove class 'active' from all articles */
+
   const activeArticles = document.querySelectorAll(".post.active");
 
   for(let activeArticle of activeArticles){
